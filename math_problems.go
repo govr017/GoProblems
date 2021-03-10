3886c5f0510a3d37b53276e20dbd863853eebf9f
govr017 <govr017@gmail.com>
2021-03-10 12:38:43 +0500

    Minus or plus options

--- math_problems.go
+++ math_problems.go
@@ -8,20 +8,36 @@ import (
 
 func main() {
 
-	colorRed := "\033[31m"
-	colorGreen := "\033[32m"
+	const COLORRED = "\033[31m"
+	const COLORGREEN = "\033[32m"
 
 	rand.Seed(time.Now().UnixNano())
 	var answer int
 	var x = rand.Intn(10)
 	var y = rand.Intn(10)
+	var method int
 
-	fmt.Println("Whats", x, "", "+", "", y)
-	fmt.Scanln(&answer)
-	if answer == x+y {
-		fmt.Println(string(colorGreen), "Good job!")
+
+  fmt.Println("Hey choose minus(1) or plus(2) 1/2?")
+	fmt.Scanln(&method)
+
+
+  if method == 1 {
+		fmt.Println("Whats", x, "", "-", "", y)
+		fmt.Scanln(&answer)
+		if answer == x-y {
+			fmt.Println(string(COLORGREEN), "Good job!")
+		} else {
+			fmt.Println(string(COLORRED), "Too bad!")
+		}
 	} else {
-		fmt.Println(string(colorRed), "Too bad!")
+		fmt.Println("Whats", x, "", "+", "", y)
+		fmt.Scanln(&answer)
+		if answer == x+y {
+			fmt.Println(string(COLORGREEN), "Good job!")
+		} else {
+			fmt.Println(string(COLORRED), "Too bad!")
+		}
 	}
 
 }

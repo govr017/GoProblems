5fbb051114ce184dbe156bb615199698ca32969f
RaSla <rasla@mail.ru>
2021-03-11 21:55:19 +0500

    feat: define var_max

--- math_problems.go
+++ math_problems.go
@@ -11,13 +11,25 @@ const COLOR_RED = "\033[31m"
 const COLOR_GREEN = "\033[32m"
 const COLOR_YELLOW = "\033[33m"
 const COLOR_BLUE = "\033[34m"
+const VAR_MAX_DEFAULT = 10
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	var answer int
-	var x = rand.Intn(10)
-	var y = rand.Intn(10)
+	var var_max int // maximum value of each number
+	var x int
+	var y int
 	var method int
+
+	fmt.Printf("Set MAXIMUM number for values (default = %d): %s", VAR_MAX_DEFAULT, COLOR_YELLOW)
+	_, err := fmt.Scanln(&var_max)
+	if err != nil {
+		var_max = VAR_MAX_DEFAULT
+		fmt.Print("var_max = ", var_max)
+	}
+	fmt.Println(COLOR_DEFAULT)
+	x = rand.Intn(var_max)
+	y = rand.Intn(var_max)
 
 	fmt.Print("Choose Math-method: minus (1) or plus (2): ", COLOR_YELLOW)
 	fmt.Scanln(&method)
